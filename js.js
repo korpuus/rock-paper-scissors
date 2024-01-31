093d612cd0b49ef11498dd083a030cc41c3a2e62
korpuus <korpus.kuba@gmail.com>
2024-01-31 19:06:00 +0100

    added getPlayerChoice function

--- js.js
+++ js.js
@@ -1,19 +1,43 @@
-//random output of rock, paper or scissors
+//function called getComputerChoice that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’. We’ll use this function in the game to make the computer’s play.
+
 function getComputerChoice () {
   if (Math.random() < 1/3) {
-    let rock = 'Rock';
+    const rock = 'Rock';
     return rock;
   }
   else if (Math.random() >= 1/3 && Math.random() <= 2/3) {
-    let paper = 'Paper';
+    const paper = 'Paper';
     return paper;
   }
   else {
-    let scissors = 'Scissors';
+    const scissors = 'Scissors';
     return scissors;
   }
 }
 
-console.log(getComputerChoice());
 
-//Single round of game. Takes2 parameters ---> playerSelection and computerSelection 
+
+//Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner or tie of the round like so: "You Lose! Paper beats Rock"
+
+function getPlayerChoice() {
+  const playerChoice = prompt('Pick Rock, Paper or Scissors: ');
+   
+  if (playerChoice.toLocaleLowerCase() === 'rock') {
+    const rock = 'Rock';
+    return rock;
+  }
+  else if (playerChoice.toLocaleLowerCase() === 'paper') {
+    const paper = 'Paper';
+    return paper;
+  }
+  else if (playerChoice.toLocaleLowerCase() === 'scissors') {
+    const scissors = 'Scissors';
+    return scissors;
+  }
+  else {
+    const invalidEntry = 'Invalid entry, try again';
+    return invalidEntry;
+  }
+}
+
+console.log(getPlayerChoice());
